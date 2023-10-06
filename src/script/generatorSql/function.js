82634798b6b63564b29b97e4f4f5c4e75a4c1c3e
maks <maks>
2023-10-06 21:33:40 +0400

    generatorSql insert

--- src/script/generatorSql/function.js
+++ src/script/generatorSql/function.js
@@ -1,4 +1,4 @@
-const { schemaAndTable } = require("./libs");
+const { schemaAndTable, columnString } = require("./libs");
 
 const createDropFun = (config, string) => {
     return `drop function if exists ${schemaAndTable(config)}${string};\n`;
@@ -7,12 +7,34 @@ const createDropFun = (config, string) => {
 const createFun = (config, string) => {
     return `create or replace function ${schemaAndTable(config)}${string}(\n`;
 }
-
+const createFunEnd = () => {
+    return `\n    end;
+$function$;`;
+}
+const createFunMetaData = () => {
+    return `    language  plpgsql
+    as $function$
+    begin
+    `;
+}
+const generatorParamsOutInsert = () => {
+    return `
+    out id_ int,
+    out result_ json`
+}
 const createFunInsert = (config) => {
     let result = "";
     const name = "_insert"
     result += createDropFun(config, name);
     result += createFun(config, name);
+    result += generatorAutoParamsIn(config);
+    result += generatorParamsOutInsert();
+    result += `\n)\n`;
+    result += createFunMetaData();
+    result += `    insert into ${schemaAndTable(config)} (${columnString(config.table.column)})`;
+    result += `\n        values (${columnString(config.table.column, "_")})`;
+    result += `\n        returning id into id_;`;
+    result += createFunEnd();
     return result;
 }
 
@@ -29,7 +51,6 @@ const generatorAutoParamsIn = (config) => {
     }
 
     result = result.slice(0, result.length - 2);
-    result += "\n)\n";
     return result;
 }
 
@@ -40,7 +61,6 @@ module.exports = {
         let result = "";
         result += "\n-- function\n\n";
         result += createFunInsert(config);
-        result += generatorAutoParamsIn(config);
         return result;
     }
 }
--- src/script/generatorSql/libs.js
+++ src/script/generatorSql/libs.js
@@ -1,5 +1,19 @@
 module.exports = {
     schemaAndTable: function schemaAndTable(config) {
         return `${config.schema.name}.${config.table.name}`;
+    },
+    columnString(columns, pre_varchar) {
+        let result = "";
+        for (const column of columns) {
+            if (column.name && !column.ai) {
+                if (pre_varchar) {
+                    result += `${pre_varchar}${column.name}, `;
+                } else {
+                    result += `${column.name}, `;
+                }
+            }
+        }
+        result = result.slice(0, result.length - 2);
+        return result;
     }
 }
